5a01dc399b086fd50d52457c61ef6a7672446324
Andy Bonner <abonner@duke.edu>
2017-12-13 12:32:30 -0500

    enable routing

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 // require express etc.
 const express = require("express");
 const mongoose = require("mongoose");
+const router = require('./router');
 
 
 // initialize express
 const app = express();
 // specify PORT with local and production
 const PORT = process.env.PORT || 8080;
+
+// Serve up static assets
+app.use(express.static("client/build"));
+
+// use router
+router(app);
 
 // connect to db
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/spellingbee", {
