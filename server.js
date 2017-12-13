3572512c32ee1d13ef10eb1760ede7c9bc773acd
Andy Bonner <abonner@duke.edu>
2017-12-13 12:22:09 -0500

    set up db

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 // require express etc.
 const express = require("express");
+const mongoose = require("mongoose");
+
 
 // initialize express
 const app = express();
-
 // specify PORT with local and production
 const PORT = process.env.PORT || 8080;
+
+// connect to db
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/spellingbee", {
+  useMongoClient: true
+});
 
 // Start the API server
 app.listen(PORT, function() {
